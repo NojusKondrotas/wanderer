40aaf3806c5e50cf143e17739d7355c46425da6e
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-08-23 14:30:13 +0300

    Fix random number generator bounds

--- runtime/clipboard.js
+++ runtime/clipboard.js
@@ -1,11 +1,15 @@
 let currClipboardID = 0
 
 function generateRandom(minRange = 0x1000, maxRange = 0xffffffff){
-    return Math.floor(Math.random() * maxRange + minRange)
+    return Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange
+}
+
+function convertToString(num, base = 10){
+    return num.toString(base)
 }
 
 function IDClipboardContent(content, minRange = 0x1000, maxRange = 0xffffffff){
-    currClipboardID = generateRandom(minRange, maxRange).toString(16)
+    currClipboardID = convertToString(generateRandom(minRange, maxRange), 16)
     
     let text = `[${currClipboardID}]` + content
     
