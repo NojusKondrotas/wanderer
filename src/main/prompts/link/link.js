36b368f8dd3455d7438ed6de7660db8846cee82e
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-10-18 12:18:24 +0300

    Include additional scripts, add window cover placeholder

--- src/main/prompts/link/link.js
+++ src/main/prompts/link/link.js
@@ -1,5 +1,7 @@
 initTitlebar();
 
+StatesHandler.isPromptLink = true
+
 (async () => {
   async function getWebviewLink() {
     return await window.wandererAPI.getLink()
@@ -8,6 +10,21 @@ initTitlebar();
   const link = await getWebviewLink()
   document.getElementById('webview').setAttribute('src', link)
 })()
+
+const cover = document.getElementById('cover')
+
+cover.addEventListener('mousemove', (e) => {
+  console.log('hi')
+    mouseMove_LinkMoveHandler(e)
+})
+
+cover.addEventListener('mousedown', (e) => {
+    mouseDown_LinkMoveHandler(e)
+})
+
+cover.addEventListener('mouseup', (e) => {
+    mouseUp_LinkMoveHandler(e)
+})
 
 function closeWindow(){
     window.wandererAPI.closeWindow()
