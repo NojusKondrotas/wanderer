8f2f866b80eba94e2e459b53909ded6a50d921e8
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-10-25 20:48:30 +0300

    Zoom towards mouse position

--- src/ui/zoom-in-whiteboard.js
+++ src/ui/zoom-in-whiteboard.js
@@ -1,12 +1,29 @@
 let zoomFactor = 1.0
-let zoomStep = 1.4
+const zoomStep = 1.4
+const boardOffset = { x: 0, y: 0 }
 
 function zoomInWhiteboard(mousePos){
+    const oldZoom = zoomFactor
     zoomFactor = Math.min(zoomFactor * zoomStep, 8)
-    parentWhiteboard.style.transform = `scale(${zoomFactor})`
+
+    const scaleChange = zoomFactor / oldZoom
+
+    boardOffset.x = mousePos.x - scaleChange * (mousePos.x - boardOffset.x)
+    boardOffset.y = mousePos.y - scaleChange * (mousePos.y - boardOffset.y)
+
+    parentWhiteboard.style.transformOrigin = '0 0'
+    parentWhiteboard.style.transform = `translate(${boardOffset.x}px, ${boardOffset.y}px) scale(${zoomFactor})`
 }
 
 function zoomOutWhiteboard(mousePos){
+    const oldZoom = zoomFactor
     zoomFactor = Math.max(zoomFactor / zoomStep, 0.1)
-    parentWhiteboard.style.transform = `scale(${zoomFactor})`
+
+    const scaleChange = zoomFactor / oldZoom
+
+    boardOffset.x = mousePos.x - scaleChange * (mousePos.x - boardOffset.x)
+    boardOffset.y = mousePos.y - scaleChange * (mousePos.y - boardOffset.y)
+
+    parentWhiteboard.style.transformOrigin = '0 0'
+    parentWhiteboard.style.transform = `translate(${boardOffset.x}px, ${boardOffset.y}px) scale(${zoomFactor})`
 }
