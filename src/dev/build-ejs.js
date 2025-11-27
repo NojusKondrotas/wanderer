2fc3011dd786308a8d29c9d07b8269679eba5c4d
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-11-27 23:38:35 +0200

    Fix ejs build: update relative filepaths, build notepad-index.ejs

--- src/dev/build-ejs.js
+++ src/dev/build-ejs.js
@@ -4,14 +4,19 @@ import ejs from 'ejs'
 
 async function build() {
     const distDir = './src/main'
+    const srcNp = path.join('./src/dev/', 'notepad-index.ejs')
     const srcWb = path.join('./src/dev/', 'whiteboard-index.ejs')
-    const srcLink = path.join('./src/dev/', 'link.ejs')
-    const srcFirstT = path.join('./src/dev/', 'prompt-first-time.ejs')
+    const srcFirstT = path.join('./src/dev/prompts/', 'prompt-first-time.ejs')
+    const srcLink = path.join('./src/dev/prompts/', 'prompt-link.ejs')
+
+    const destNp = path.join(distDir, 'notepad-index.html')
     const destWb = path.join(distDir, 'whiteboard-index.html')
-    const destLink = path.join(distDir, 'prompts/link', 'link.html')
+    const destLink = path.join(distDir, 'prompts/link', 'prompt-link.html')
     const destFirstT = path.join(distDir, 'prompts/first-time', 'prompt-first-time.html')
 
     let html
+    html = await ejs.renderFile(srcNp, {})
+    await fs.writeFile(destNp, html)
     html = await ejs.renderFile(srcWb, {})
     await fs.writeFile(destWb, html)
     html = await ejs.renderFile(srcLink, {})
