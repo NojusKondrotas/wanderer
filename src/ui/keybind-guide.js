2d9fceb62577a157126a6b3a0f25867d817f2cbe
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-12-20 12:14:50 +0200

    Style keybind close button with an img

--- src/ui/keybind-guide.js
+++ src/ui/keybind-guide.js
@@ -1,46 +1,47 @@
-const keybindGuide = document.getElementById('keybind-guide')
-const keybindGuideCloseCtrl = document.getElementById('keybind-guide-close')
-const keybindContainer = document.getElementById('keybind-container')
-const keybindContainerChildren = keybindContainer.children
+const keybindGuide = document.getElementById('keybind-guide');
+const keybindGuideCloseCtrl = document.getElementById('keybind-guide-close');
+const keybindGuideCloseImg = document.getElementById('kbn-gd-cl-img');
+const keybindContainer = document.getElementById('keybind-container');
+const keybindContainerChildren = keybindContainer.children;
 
 function keybindGuide_MouseOverHandler(){
     for(let child of keybindContainerChildren){
-        child.style.color = 'rgba(126, 135, 135, 1)'
+        child.style.color = 'rgba(126, 135, 135, 1)';
     }
-    keybindGuideCloseCtrl.style.color = 'rgba(126, 135, 135, 1)'
+    keybindGuideCloseImg.style.opacity = '1';
 }
 
 function keybindGuide_MouseOutHandler(){
-    keybindGuideCloseCtrl.style.color = 'rgba(126, 135, 135, 0)'
+    keybindGuideCloseImg.style.opacity = '0';
     for(let child of keybindContainerChildren){
-        child.style.color = 'rgba(126, 135, 135, 0.33)'
+        child.style.color = 'rgba(126, 135, 135, 0.33)';
     }
 }
 
 function handleKeybindGuideAppearance(flag){
     if(flag){
-        keybindGuide.style.pointerEvents = "auto"
-        keybindGuideCloseCtrl.style.color = 'rgba(126, 135, 135, 0)'
+        keybindGuide.style.pointerEvents = "auto";
+        keybindGuideCloseImg.style.color = '0';
         for(let child of keybindContainerChildren){
-            child.style.color = 'rgba(126, 135, 135, 0.33)'
+            child.style.color = 'rgba(126, 135, 135, 0.33)';
         }
-        keybindGuide.addEventListener('mouseover', keybindGuide_MouseOverHandler)
+        keybindGuide.addEventListener('mouseover', keybindGuide_MouseOverHandler);
 
-        keybindGuide.addEventListener('mouseout', keybindGuide_MouseOutHandler)
+        keybindGuide.addEventListener('mouseout', keybindGuide_MouseOutHandler);
     }else{
-        keybindGuide.style.pointerEvents = "none"
-        keybindGuideCloseCtrl.style.color = 'rgba(126, 135, 135, 0)'
+        keybindGuide.style.pointerEvents = "none";
+        keybindGuideCloseImg.style.opacity = '0';
         for(let child of keybindContainerChildren){
-            child.style.color = 'rgba(126, 135, 135, 0)'
+            child.style.color = 'rgba(126, 135, 135, 0)';
         }
-        keybindGuide.removeEventListener('mouseover', keybindGuide_MouseOverHandler)
+        keybindGuide.removeEventListener('mouseover', keybindGuide_MouseOverHandler);
 
-        keybindGuide.removeEventListener('mouseout', keybindGuide_MouseOutHandler)
+        keybindGuide.removeEventListener('mouseout', keybindGuide_MouseOutHandler);
     }
 }
 
 document.getElementById('keybind-guide-close').addEventListener('mousedown', (e) => {
-    e.stopPropagation()
-    handleKeybindGuideAppearance(false)
-    keybindGuide.style.display = 'none'
+    e.stopPropagation();
+    handleKeybindGuideAppearance(false);
+    keybindGuide.style.display = 'none';
 })
