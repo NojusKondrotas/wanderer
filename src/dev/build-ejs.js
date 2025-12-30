b2e7188ea70d4abe033318335d017f74100a6567
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-12-30 23:10:08 +0200

    Update EJS build script

--- src/dev/build-ejs.js
+++ src/dev/build-ejs.js
@@ -2,27 +2,31 @@ import fs from 'fs/promises'
 import path from 'path'
 import ejs from 'ejs'
 
-async function build() {
-    const distDir = './src/main'
-    const srcNp = path.join('./src/dev/', 'notepad-index.ejs')
-    const srcWb = path.join('./src/dev/', 'whiteboard-index.ejs')
-    const srcFirstT = path.join('./src/dev/prompts/', 'prompt-first-time.ejs')
-    const srcLink = path.join('./src/dev/prompts/', 'prompt-link.ejs')
-
-    const destNp = path.join(distDir, 'notepad-index.html')
-    const destWb = path.join(distDir, 'whiteboard-index.html')
-    const destLink = path.join(distDir, 'prompts/link', 'prompt-link.html')
-    const destFirstT = path.join(distDir, 'prompts/first-time', 'prompt-first-time.html')
-
-    let html
-    html = await ejs.renderFile(srcNp, {})
-    await fs.writeFile(destNp, html)
-    html = await ejs.renderFile(srcWb, {})
-    await fs.writeFile(destWb, html)
-    html = await ejs.renderFile(srcLink, {})
-    await fs.writeFile(destLink, html)
-    html = await ejs.renderFile(srcFirstT, {})
-    await fs.writeFile(destFirstT, html)
+async function writeHTML(src, dest){
+    let html = await ejs.renderFile(src, {});
+    await fs.writeFile(dest, html);
 }
 
-build()
+async function build() {
+    const mainDir = './src/main';
+    const srcs = [
+        path.join('./src/dev/', 'notepad-index.ejs'),
+        path.join('./src/dev/', 'whiteboard-index.ejs'),
+        path.join('./src/dev/prompts/', 'prompt-first-time.ejs'),
+        path.join('./src/dev/prompts/', 'prompt-link.ejs'),
+        path.join('./src/dev/prompts/', 'configs.ejs')
+    ];
+    const dests = [
+        path.join(mainDir, 'notepad-index.html'),
+        path.join(mainDir, 'whiteboard-index.html'),
+        path.join(mainDir, 'prompts/first-time', 'prompt-first-time.html'),
+        path.join(mainDir, 'prompts/link', 'prompt-link.html'),
+        path.join(mainDir, 'prompts/configs', 'configs.html')
+    ];
+
+    for(let i = 0; i < srcs.length; ++i) {
+        writeHTML(srcs[i], dests[i]);
+    }
+}
+
+build();
