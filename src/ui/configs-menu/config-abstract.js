807b16453b5f1bc20ddab41fda35779cbca65d8e
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-12-31 17:40:23 +0200

    Delete references to abstractsExits

--- src/ui/configs-menu/config-abstract.js
+++ src/ui/configs-menu/config-abstract.js
@@ -6,22 +6,10 @@ const abstracts = [
     document.getElementById('cfg-abst-internal')
 ];
 
-function toggleAllConfigsAbstracts(flag) {
-    if (flag) {
-        abstracts.forEach(abstract => abstract.style.display = 'block');
-        abstractsExits.forEach(exit => exit.style.display = 'block');
-    } else {
-        abstracts.forEach(abstract => abstract.style.display = 'none');
-        abstractsExits.forEach(exit => exit.style.display = 'none');
-    }
-}
-
-function toggleSingleConfigAbstract(flag, abstractIdx) {
+function toggleConfigAbstract(flag, abstractIdx) {
     if (flag) {
         abstracts[abstractIdx].style.display = 'block';
-        abstractsExits[abstractIdx].style.display = 'block';
     } else {
         abstracts[abstractIdx].style.display = 'none';
-        abstractsExits[abstractIdx].style.display = 'none';
     }
 }
