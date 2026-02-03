85ad418bfc065b228baf2cf62f668f4b3f83d01b
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-02-03 18:29:14 +0200

    Encapsulate window positioning logic inside a class

--- src/ui/positioning/window-positioning.js
+++ src/ui/positioning/window-positioning.js
@@ -1,42 +1,44 @@
-let isDraggingWindow = false
-let isResizingWindow = false
-let windowDimensions = { width: 0, height: 0 }
-let windowCornerOffset = { x: 0, y: 0 }
+class WindowPositioningHandler {
+    static isDraggingWindow = false
+    static isResizingWindow = false
+    static windowDimensions = { width: 0, height: 0 }
+    static windowCornerOffset = { x: 0, y: 0 }
 
-function resetWindowDrag(ev){
-    isDraggingWindow = false
-    isResizingWindow = false
-    windowDimensions = { width: window.outerWidth, height: window.outerHeight }
-    windowCornerOffset = {
-        x: ev.screenX - window.screenX,
-        y: ev.screenY - window.screenY
+    static resetWindowDrag(ev){
+        this.isDraggingWindow = false
+        this.isResizingWindow = false
+        this.windowDimensions = { width: window.outerWidth, height: window.outerHeight }
+        this.windowCornerOffset = {
+            x: ev.screenX - window.screenX,
+            y: ev.screenY - window.screenY
+        }
     }
-}
 
-function moveWindow(){
-    const newX = dragStart.x - windowCornerOffset.x
-    const newY = dragStart.y - windowCornerOffset.y
+    static moveWindow(){
+        const newX = dragStart.x - this.windowCornerOffset.x
+        const newY = dragStart.y - this.windowCornerOffset.y
 
-    window.wandererAPI.moveWindow(
-        newX,
-        newY,
-        windowDimensions.width,
-        windowDimensions.height
-    )
-}
+        window.wandererAPI.moveWindow(
+            newX,
+            newY,
+            this.windowDimensions.width,
+            this.windowDimensions.height
+        )
+    }
 
-function resizeWindow(){
-    const dx = -dragDiff.x
-    const dy = -dragDiff.y
+    static resizeWindow(){
+        const dx = -dragDiff.x
+        const dy = -dragDiff.y
 
-    let { width, height } = windowDimensions
-    let newX = window.screenX
-    let newY = window.screenY
+        let { width, height } = this.windowDimensions
+        let newX = window.screenX
+        let newY = window.screenY
 
-    width += dx
-    height += dy
+        width += dx
+        height += dy
 
-    window.wandererAPI.moveWindow(newX, newY, width, height)
+        window.wandererAPI.moveWindow(newX, newY, width, height)
 
-    windowDimensions = { width, height }
+        this.windowDimensions = { width, height }
+    }
 }
