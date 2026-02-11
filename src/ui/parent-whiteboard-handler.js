1a303d7b04777e5a51fd36fb6a331b9ddf085d2e
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-02-11 14:02:31 +0200

    Delete duplicate drag callers

--- src/ui/parent-whiteboard-handler.js
+++ src/ui/parent-whiteboard-handler.js
@@ -1,18 +1,2 @@
 const wbMovement = document.getElementById('wb-movement')
 const wbZoom = document.getElementById('wb-zoom')
-
-// parentWhiteboard.addEventListener('contextmenu', (e) => {
-//     e.preventDefault()
-//     e.stopPropagation()
-//     if(StatesHandler.isWritingElement) toggleWritingMode(false, selectedElement.id)
-
-//     openNewContextMenu(e.clientX, e.clientY, gcm)
-// })
-
-wbMovement.addEventListener('mousedown', (e) => {
-    genMouseDown_WhiteboardMoveHandler(e)
-})
-
-wbMovement.addEventListener('mouseup', (e) => {
-    genMouseUp_WhiteboardMoveHandler(e)
-})
