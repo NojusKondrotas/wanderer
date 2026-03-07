7c2b8d09c4d07bcb562a6e5fad1db50524d3c28d
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-03-07 21:34:18 +0200

    Fix whiteboard zooming to account for coord space mismatch

--- src/ui/zoom-whiteboard.ts
+++ src/ui/zoom-whiteboard.ts
@@ -2,22 +2,25 @@ import { wbOffset } from "./positioning/whiteboard-positioning.js"
 import { wbZoom } from "./parent-whiteboard-handler.js"
 import { Vector2D } from "../runtime/numerics.js"
 
-export let zoomFactor = 1.0
+export let zoomFactor = 1.0;
 export const setZoomFactor = (factor: number) => zoomFactor = factor;
-export const zoomStep = 1.4
-export let boardOffset = new Vector2D(0, 0)
-export const setBoardOffset = (offset: Vector2D) => boardOffset = offset
+export const zoomStep = 1.4;
+export let boardOffset = new Vector2D(0, 0);
+export const setBoardOffset = (offset: Vector2D) => boardOffset = offset;
 
-export function zoomWhiteboard(mousePos, zoomIn = false){
+export function zoomWhiteboard(mousePos: Vector2D, zoomIn = false){
     const oldZoom = zoomFactor
     if(zoomIn)
-        zoomFactor = Math.min(zoomFactor * zoomStep, 8)
-    else zoomFactor = Math.max(zoomFactor / zoomStep, 0.1)
+        zoomFactor = Math.min(zoomFactor * zoomStep, 8);
+    else zoomFactor = Math.max(zoomFactor / zoomStep, 0.1);
 
     const scaleChange = zoomFactor / oldZoom
 
-    boardOffset.x = mousePos.x - scaleChange * (mousePos.x - boardOffset.x)
-    boardOffset.y = mousePos.y - scaleChange * (mousePos.y - boardOffset.y)
+    const mx = mousePos.x - wbOffset.x
+    const my = mousePos.y - wbOffset.y
+
+    boardOffset.x = mx - scaleChange * (mx - boardOffset.x)
+    boardOffset.y = my - scaleChange * (my - boardOffset.y)
     
     wbZoom.style.transform = `translate(${boardOffset.x}px, ${boardOffset.y}px) scale(${zoomFactor})`
 }
